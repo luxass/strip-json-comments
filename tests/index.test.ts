cf91595171205f75b5728d6b1db5a4dd3b15d788
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-11-19 13:53:02 +0100

    chore: use new flat config

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
+import { expect, it } from "vitest";
 import { strip } from "../src";
 
-test("replace comments with whitespace", () => {
+it("replace comments with whitespace", () => {
   expect(strip("//comment\n{\"a\":\"b\"}")).toBe("         \n{\"a\":\"b\"}");
   expect(strip("/*//comment*/{\"a\":\"b\"}")).toBe("             {\"a\":\"b\"}");
   expect(strip("{\"a\":\"b\"//comment\n}")).toBe("{\"a\":\"b\"         \n}");
@@ -11,7 +11,7 @@ test("replace comments with whitespace", () => {
   expect(strip("{/*comment*/\"a\":\"b\"}")).toBe("{           \"a\":\"b\"}");
 });
 
-test("remove comments", () => {
+it("remove comments", () => {
   const options = { whitespace: false };
 
   expect(strip("//comment\n{\"a\":\"b\"}", options)).toBe("\n{\"a\":\"b\"}");
@@ -23,49 +23,49 @@ test("remove comments", () => {
   expect(strip("{/*comment*/\"a\":\"b\"}", options)).toBe("{\"a\":\"b\"}");
 });
 
-test("doesn't strip comments inside strings", () => {
+it("doesn't strip comments inside strings", () => {
   expect(strip("{\"a\":\"b//c\"}")).toBe("{\"a\":\"b//c\"}");
   expect(strip("{\"a\":\"b/*c*/\"}")).toBe("{\"a\":\"b/*c*/\"}");
   expect(strip("{\"/*a\":\"b\"}")).toBe("{\"/*a\":\"b\"}");
   expect(strip("{\"\\\"/*a\":\"b\"}")).toBe("{\"\\\"/*a\":\"b\"}");
 });
 
-test("consider escaped slashes when checking for escaped string quote", () => {
+it("consider escaped slashes when checking for escaped string quote", () => {
   expect(strip("{\"\\\\\":\"https://foobar.com\"}")).toBe("{\"\\\\\":\"https://foobar.com\"}");
   expect(strip("{\"foo\\\"\":\"https://foobar.com\"}")).toBe("{\"foo\\\"\":\"https://foobar.com\"}");
 });
 
-test("line endings - no comments", () => {
+it("line endings - no comments", () => {
   expect(strip("{\"a\":\"b\"\n}")).toBe("{\"a\":\"b\"\n}");
   expect(strip("{\"a\":\"b\"\r\n}")).toBe("{\"a\":\"b\"\r\n}");
 });
 
-test("line endings - single line comment", () => {
+it("line endings - single line comment", () => {
   expect(strip("{\"a\":\"b\"//c\n}")).toBe("{\"a\":\"b\"   \n}");
   expect(strip("{\"a\":\"b\"//c\r\n}")).toBe("{\"a\":\"b\"   \r\n}");
 });
 
-test("line endings - single line block comment", () => {
+it("line endings - single line block comment", () => {
   expect(strip("{\"a\":\"b\"/*c*/\n}")).toBe("{\"a\":\"b\"     \n}");
   expect(strip("{\"a\":\"b\"/*c*/\r\n}")).toBe("{\"a\":\"b\"     \r\n}");
 });
 
-test("line endings - multi line block comment", () => {
+it("line endings - multi line block comment", () => {
   expect(strip("{\"a\":\"b\",/*c\nc2*/\"x\":\"y\"\n}")).toBe("{\"a\":\"b\",   \n    \"x\":\"y\"\n}");
   expect(strip("{\"a\":\"b\",/*c\r\nc2*/\"x\":\"y\"\r\n}")).toBe("{\"a\":\"b\",   \r\n    \"x\":\"y\"\r\n}");
 });
 
-test("line endings - works at EOF", () => {
+it("line endings - works at EOF", () => {
   const options = { whitespace: false };
   expect(strip("{\r\n\t\"a\":\"b\"\r\n} //EOF")).toBe("{\r\n\t\"a\":\"b\"\r\n}      ");
   expect(strip("{\r\n\t\"a\":\"b\"\r\n} //EOF", options)).toBe("{\r\n\t\"a\":\"b\"\r\n} ");
 });
 
-test("handles weird escaping", () => {
+it("handles weird escaping", () => {
   expect(strip(String.raw`{"x":"x \"sed -e \\\"s/^.\\\\{46\\\\}T//\\\" -e \\\"s/#033/\\\\x1b/g\\\"\""}`)).toBe(String.raw`{"x":"x \"sed -e \\\"s/^.\\\\{46\\\\}T//\\\" -e \\\"s/#033/\\\\x1b/g\\\"\""}`);
 });
 
-test("strips trailing commas", () => {
+it("strips trailing commas", () => {
   expect(strip("{\"x\":true,}", { trailingCommas: true })).toBe("{\"x\":true }");
   expect(strip("{\"x\":true,}", { trailingCommas: true, whitespace: false })).toBe("{\"x\":true}");
   expect(strip("{\"x\":true,\n  }", { trailingCommas: true })).toBe("{\"x\":true \n  }");
@@ -75,7 +75,7 @@ test("strips trailing commas", () => {
   expect(strip("{\n  \"array\": [\n    true,\n    false /* comment */ ,\n /*comment*/ ],\n}", { trailingCommas: true, whitespace: false })).toBe("{\n  \"array\": [\n    true,\n    false  \n  ]\n}");
 });
 
-test.fails("handles malformed block comments", () => {
+it.fails("handles malformed block comments", () => {
   expect(strip("[] */")).toBe("[] */");
   expect(strip("[] /*")).toBe("[] /*"); // Fails
 });
